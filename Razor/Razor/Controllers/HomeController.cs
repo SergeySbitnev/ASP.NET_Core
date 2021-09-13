6f3e0adf33fceb43a7afdaa73c901d9d64f571b2
Sergey Sbitnev <sergeysbitnev@mail.ru>
2021-09-13 22:23:31 +0300

    13.09.2021

--- Razor/Razor/Controllers/HomeController.cs
+++ Razor/Razor/Controllers/HomeController.cs
@@ -11,27 +11,17 @@ namespace Razor.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public ViewResult Index()
         {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            Product [] array =
+            {
+                 new Product {Name = "Kayak", Price = 275M},
+                 new Product {Name = "Lifejacket", Price = 48.95M},
+                 new Product {Name = "Soccer ball", Price = 19.50M},
+                 new Product {Name = "Corner flag", Price = 34.95M},
+            };
+            return View(array);
         }
     }
 }
---
+++ WorkingWithVisualStudio/WorkingWithVisualStudio/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkingWithVisualStudio.Models;
+
+namespace WorkingWithVisualStudio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View(SimpleRepository.SharedRepository.Products);
+    }
+}
