168955b05b8eb85e347ba270c6beab67860ec5de
Sergey Sbitnev <sergeysbitnev@mail.ru>
2021-09-20 21:39:37 +0300

    20.09.2021

---
+++ EnergoUchet_Core/EnergoUchet_Core/Controllers/DispetcherDataController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EnergoUchet_Core.Models;
+
+namespace EnergoUchet_Core.Controllers
+{
+    public class DispetcherDataController : Controller
+    {
+        private IDispetcherDataRepository dispetcherDataRepository;
+
+        public DispetcherDataController (IDispetcherDataRepository repo)
+        {
+            dispetcherDataRepository = repo;
+        }
+
+        public ViewResult List() => View(dispetcherDataRepository.DispetcherDatas); 
+    }
+}
---
+++ EnergoUchet_Core/EnergoUchet_Core/Controllers/HeatpowerDataController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EnergoUchet_Core.Models;
+
+namespace EnergoUchet_Core.Controllers
+{
+    public class HeatpowerDataController : Controller
+    {
+        private IHeatpowerDataRepository heatpowerDataRepository;
+
+        public HeatpowerDataController (IHeatpowerDataRepository repo)
+        {
+            heatpowerDataRepository = repo;
+        }
+
+        public ViewResult List() => View(heatpowerDataRepository.HeatpowerDatas);
+    }
+}
