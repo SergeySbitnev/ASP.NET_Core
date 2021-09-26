3bc7734727baf94fad0f956ed3342844abbc447a
Sergey Sbitnev <sergeysbitnev@mail.ru>
2021-09-26 20:14:00 +0300

    26.09.2021

---
+++ EnergoUchet_Core/EnergoUchet_Core/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EnergoUchet_Core.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<DispetcherData> DispetcherDatas { get; set; }
+    }
+}
---
+++ EnergoUchet_Core/EnergoUchet_Core/Models/EFDispetcherDataRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EnergoUchet_Core.Models
+{
+    public class EFDispetcherDataRepository : IDispetcherDataRepository
+    {
+        private ApplicationDbContext context;
+        public EFDispetcherDataRepository (ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+        public IQueryable<DispetcherData> DispetcherDatas => context.DispetcherDatas;
+    }
+}
